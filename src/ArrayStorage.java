541c665d2fac6d2dc2a21a6151641f1651fdb7c3
Andrei Shumov <staydoomed@gmail.com>
2023-01-04 18:03:46 +0300

    added methods in ArrayStorage:save, get, delete, clear, getAll, size; added int size field in ArrayStorage; added getUuid() in Resume;

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,66 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-
+    private int size=0;
     void clear() {
+        for(int i =0;i<size();i++){
+            storage[i]=null;
+        }
+        size=0;
     }
 
     void save(Resume r) {
+        if (size()==storage.length){// if no more place in storage
+            Resume[] newStorage= new Resume[storage.length*2];
+            for(int i=0;i<size();i++){
+                newStorage[i]=storage[i];
+            }
+            storage=newStorage;
+        }
+        for(int i=0;i<size();i++){
+            if(r.getUuid().equals(storage[i].getUuid())){
+                return; //such uuid already in storage
+            }
+        }
+        storage[size]=r;
+        size++;
+
     }
 
     Resume get(String uuid) {
-        return null;
+        for(int i=0;i<size();i++){
+            if(uuid.equals(storage[i].getUuid())){
+                return storage[i];
+            }
+
+        }
+        return null; //no resume with such uuid
     }
 
     void delete(String uuid) {
+        for(int i=0;i<size();i++){
+            if(uuid.equals(storage[i].getUuid())){
+                storage[i]=storage[size-1];
+                storage[size-1]=null;
+                size=size-1;
+            }
+
+        }
+
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] noNullStorage = new Resume[size];
+        for(int i=0;i<size();i++){
+            noNullStorage[i]=storage[i];
+        }
+        return noNullStorage;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
