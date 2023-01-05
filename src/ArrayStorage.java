3c01b74275f872564a61d66e0bb566278f0894d3
Andrei Shumov <staydoomed@gmail.com>
2023-01-05 19:43:51 +0300

    ArrayStorage: code formatted; ArrayStorage: added "break" in delete method; ArrayStorage: all methods are now using field "size", not size() method; ArrayStorage:  save(Resume r)  method now prints "Out of bound!" if storage  is filled.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,65 +1,61 @@
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    private int size=0;
+    private int size;
+
     void clear() {
-        for(int i =0;i<size();i++){
-            storage[i]=null;
+        for (int i = 0; i < size(); i++) {
+            storage[i] = null;
         }
-        size=0;
+        size = 0;
     }
 
     void save(Resume r) {
-        if (size()==storage.length){// if no more place in storage
-            Resume[] newStorage= new Resume[storage.length*2];
-            for(int i=0;i<size();i++){
-                newStorage[i]=storage[i];
-            }
-            storage=newStorage;
+        if (size == storage.length) {// if no more place in storage
+            System.out.println("Out of bound!");
+            return;
         }
-        for(int i=0;i<size();i++){
-            if(r.getUuid().equals(storage[i].getUuid())){
+        for (int i = 0; i < size; i++) {
+            if (r.getUuid().equals(storage[i].getUuid())) {
                 return; //such uuid already in storage
             }
         }
-        storage[size]=r;
+        storage[size] = r;
         size++;
-
     }
 
     Resume get(String uuid) {
-        for(int i=0;i<size();i++){
-            if(uuid.equals(storage[i].getUuid())){
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
                 return storage[i];
             }
-
         }
         return null; //no resume with such uuid
     }
 
     void delete(String uuid) {
-        for(int i=0;i<size();i++){
-            if(uuid.equals(storage[i].getUuid())){
-                storage[i]=storage[size-1];
-                storage[size-1]=null;
-                size=size-1;
+        for (int i = 0; i < size; i++) {
+            if (uuid.equals(storage[i].getUuid())) {
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
+                size--;
+                break;
             }
-
         }
-
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        Resume[] noNullStorage = new Resume[size];
-        for(int i=0;i<size();i++){
-            noNullStorage[i]=storage[i];
+        Resume[] resumes = new Resume[size];
+        for (int i = 0; i < size(); i++) {
+            resumes[i] = storage[i];
         }
-        return noNullStorage;
+        return resumes;
     }
 
     int size() {
